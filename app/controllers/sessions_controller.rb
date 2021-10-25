11ad60d28a5e230887f7eb09d2c8f6ac29909141
vkub1 <kubliyvlad@gmail.com>
2021-10-25 12:24:40 -0700

    TDD sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
 
     def new
-        
+
+    end
+
+    def create
+        @user = User.find_by_email params[:email]
+        if @user && @user.authenticate(params[:password])
+            flash[:success] = "You have logged in succesfully"
+            redirect_to ideas_path
+        else
+            flash[:alert] = "Wrong email or password!"
+            render :new
+        end
     end
     
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,5 +7,45 @@ RSpec.describe SessionsController, type: :controller do
             expect(response).to(render_template(:new))  
         end
     end
+
+    describe "#create" do
+        before do
+            @user = FactoryBot.create(:user)
+        end
+        
+        context "with valid params" do
+            before do
+                post(:create, params: {email: @user.email, password: @user.password})
+            end
+
+            it "should redirect to the ideas index page" do
+                expect(response).to(redirect_to(ideas_path))  
+            end
+            
+            it "should set a flash success message" do
+                expect(flash[:success]).to(be)  
+            end
+            
+        end
+        
+        context "with invalid params" do
+            before do
+                post(:create, params: {email: @user.email, password: nil})
+            end
+
+            it "should render the new template" do
+                expect(response).to(render_template(:new))  
+            end
+
+            it "should render set a flash alert message" do
+                expect(flash[:alert]).to(be)
+            end
+        end
+    end
+
+    
+    
+    
+    
     
 end

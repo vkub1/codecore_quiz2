09ed31892a895cebac06ad847be827b2a07f7c91
vkub1 <kubliyvlad@gmail.com>
2021-10-25 13:15:23 -0700

    TDD user authentication for idea controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
     def new
 
     end
@@ -8,11 +7,16 @@ class SessionsController < ApplicationController
         @user = User.find_by_email params[:email]
         if @user && @user.authenticate(params[:password])
             flash[:success] = "You have logged in succesfully"
+            session[:user_id] = @user.id
             redirect_to ideas_path
         else
             flash[:alert] = "Wrong email or password!"
             render :new
         end
     end
-    
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to ideas_path
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "You have signed up successfuly!"
             redirect_to ideas_path    
         else

38bc8246920bd8e4ebed5c47b9b318c48b769450
vkub1 <kubliyvlad@gmail.com>
2021-10-25 10:01:59 -0700

    Added Idea model, model validations and put ideas into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Idea.destroy_all
+
+5.times do
+    created_at = Faker::Date.backward(days:365 * 2)
+    Idea.create(
+        title: Faker::Company.catch_phrase,
+        description: Faker::Lorem.paragraph(sentence_count: 8),
+        created_at: created_at
+    )
+end
+
